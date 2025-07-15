98f744eddcf798a39cd452b58005c742b7db1b91
Enriiiicoo <anasaboudrar24@gmail.com>
2025-07-15 21:48:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const {
   Events
 } = require('discord.js');
 
-const mysql = require('mysql2/promise');
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
